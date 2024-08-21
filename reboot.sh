54937b57030b7153a0429db1b3ef8e9840b2f3f2
eirikpa <156180377+eirikpa@users.noreply.github.com>
2024-08-21 22:48:42 +0200

    Create reboot.sh

---
+++ reboot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Desired reboot time (24-hour format)
+REBOOT_TIME="06:00"
+
+# Get the current time in HH:MM format
+CURRENT_TIME=$(date +"%H:%M")
+
+# Wait until the desired time to reboot
+while [ "$CURRENT_TIME" != "$REBOOT_TIME" ]; do
+    sleep 30
+    CURRENT_TIME=$(date +"%H:%M")
+done
+
+# Reboot the system
+sudo reboot
