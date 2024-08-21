ae22c62eafde8bfddc7d2661bc6a2ee6d0d87c35
eirikpa <156180377+eirikpa@users.noreply.github.com>
2024-08-21 22:59:43 +0200

    Update systemupdate.sh

--- systemupdate.sh
+++ systemupdate.sh
@@ -1,40 +1,31 @@
 #!/bin/bash
 
-pkill zenity
-
-# Display a Zenity message informing the user of the process
+# Display a Zenity progress dialog with a consistent message
 (
   echo "0"  # Start the progress at 0%
-  echo "# The player hasn't been online for a while and needs to run a few updates and then reboot before launching your screen. The whole process will take less than 5 minutes."
   sleep 1
 
   # Run the update commands
   echo "20"  # Progress 20%
-  echo "# Updating package lists..."
   sudo apt update
 
   echo "40"  # Progress 40%
-  echo "# Upgrading installed packages..."
   sudo apt upgrade -y
 
   echo "60"  # Progress 60%
-  echo "# Fixing broken dependencies..."
   sudo apt --fix-broken install -y
 
   echo "80"  # Progress 80%
-  echo "# Removing unnecessary packages..."
   sudo apt autoremove -y
 
   echo "90"  # Progress 90%
-  echo "# Setting the Plymouth theme..."
   sudo plymouth-set-default-theme -R loopsign
 
   echo "100"  # Complete the progress at 100%
-  echo "# Rebooting the system..."
-  sleep 2
+  sleep 1
 ) | zenity --progress \
            --title="System Update in Progress" \
-           --text="The player hasn't been online for a while and needs to run a few updates and then reboot before launching your screen. The whole process will take less than 5 minutes.\n\nThank you for your patience!" \
+           --text="The player hasn't been online for a while\nand needs to run a few updates\nand then reboot before launching your screen.\n\nThe whole process will take less than 5 minutes.\n\nThank you for your patience!" \
            --percentage=0 \
            --auto-close \
            --no-cancel
