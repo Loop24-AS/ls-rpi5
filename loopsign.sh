bb04d2763e438f8a4d26d6a5521b1420f69442e5
eirikpa <156180377+eirikpa@users.noreply.github.com>
2024-08-21 12:23:56 +0200

    Update loopsign.sh

--- loopsign.sh
+++ loopsign.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Start Chromium in kiosk mode and navigate to LoopSign URL
-echo "Chromium command started"
 
 # Read the hash from hash.txt on the Desktop
 HASH=$(cat /home/loopsign/Desktop/.hash.txt)
 
 # Launch Chromium in kiosk mode with the specified URL
-export DISPLAY=:0.0
+#export DISPLAY=:0.0
 /usr/bin/chromium-browser --start-maximized --start-fullscreen --incognito --disable-desktop-notifications --no-first-run https://edit.loopsign.eu/hash/$HASH
