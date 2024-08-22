24dc75ff8b04d6a14503a999119b3e3711c6d3f8
eirikpa <156180377+eirikpa@users.noreply.github.com>
2024-08-22 22:38:07 +0200

    Update hidecursor.sh

--- hidecursor.sh
+++ hidecursor.sh
@@ -5,10 +5,10 @@ sudo apt install -y interception-tools interception-tools-compat
 sudo apt install -y cmake
 cd ~
 git clone git@github.com:eirikpa/ls-rpi5-hidecursor.git
-sudo cp /home/$USER/hideaway/config.yaml /etc/interception/udevmon.d/config.yaml
-cd hideaway
+sudo cp /home/$USER/ls-rpi5-hidecursor/config.yaml /etc/interception/udevmon.d/config.yaml
+cd ls-rpi5-hidecursor
 cmake -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build
-sudo cp /home/$USER/hideaway/build/hideaway /usr/bin
+sudo cp /home/$USER/ls-rpi5-hidecursor/build/hideaway /usr/bin
 sudo chmod +x /usr/bin/hideaway
 sudo systemctl restart udevmon
