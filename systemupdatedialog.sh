d41157cdb4a2c4a17287cecf134d3cd7000c3a03
eirikpa <156180377+eirikpa@users.noreply.github.com>
2025-01-29 14:34:44 +0100

    Update systemupdatedialog.sh
    Simplifying the system update at first boot.

--- systemupdatedialog.sh
+++ systemupdatedialog.sh
@@ -1,34 +1,19 @@
 #!/bin/bash
 
-# Display a Zenity progress dialog with a consistent message
+# Define log file (will be overwritten on each run)
+LOGFILE="/var/log/player_update.log"
+
+# Show a Zenity info dialog while running updates in the background
+zenity --info \
+  --title="System Update in Progress" \
+  --text="The player hasn't been online for a while\nand needs to run a few updates\nand then reboot before launching your screen.\n\nThe whole process will probably take less than 5 minutes.\n\nThank you for your patience!" &
+
+# Run updates in the background and log output while displaying it
 (
-  echo "0"  # Start the progress at 0%
-  sleep 1
-
-  # Run the update commands
-  echo "20"  # Progress 20%
-  sudo apt update
-
-  echo "40"  # Progress 40%
-  sudo apt upgrade -y
-
-  echo "60"  # Progress 60%
-  sudo apt --fix-broken install -y
-
-  echo "80"  # Progress 80%
-  sudo apt autoremove -y
-
-  echo "90"  # Progress 90%
+  sudo apt-get update
+  sudo apt-get upgrade -y
+  sudo apt-get --fix-broken install -y
+  sudo apt-get autoremove -y
   sudo plymouth-set-default-theme -R loopsign
-
-  echo "100"  # Complete the progress at 100%
-  sleep 1
-) | zenity --progress \
-           --title="System Update in Progress" \
-           --text="The player hasn't been online for a while\nand needs to run a few updates\nand then reboot before launching your screen.\n\nThe whole process will take less than 5 minutes.\n\nThank you for your patience!" \
-           --percentage=0 \
-           --auto-close \
-           --no-cancel
-
-# Reboot the system after the process is complete
-sudo reboot
+  sudo reboot
+) 2>&1 | sudo tee "$LOGFILE" &
