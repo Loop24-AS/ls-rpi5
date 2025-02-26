d427b3454579dba315550f9b59295eb4de6273b0
eirikpa <156180377+eirikpa@users.noreply.github.com>
2025-02-26 09:51:36 +0100

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Calculate seconds until 6 AM tomorrow
-CURRENT_HOUR=$(date +"%H")
-CURRENT_MINUTE=$(date +"%M")
-CURRENT_SECOND=$(date +"%S")
+CURRENT_HOUR=$(date +"%-H")
+CURRENT_MINUTE=$(date +"%-M")
+CURRENT_SECOND=$(date +"%-S")
 
 HOURS_UNTIL_6AM=$(( (24 - CURRENT_HOUR + 6) % 24 ))
 SECONDS_UNTIL_6AM=$(( HOURS_UNTIL_6AM * 3600 - CURRENT_MINUTE * 60 - CURRENT_SECOND ))
