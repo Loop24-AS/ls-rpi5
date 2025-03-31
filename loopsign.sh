9e9e862ce03ac2e9086a129dc170d1db9d224be0
eirikpa <156180377+eirikpa@users.noreply.github.com>
2025-03-31 13:19:09 +0200

    Update loopsign.sh

--- loopsign.sh
+++ loopsign.sh
@@ -19,4 +19,4 @@ rm -f \
 HASH=$(cat /home/loopsign/Desktop/.hash.txt)
 
 # Launch Chromium in kiosk mode with the specified URL
-chromium-browser --disable-media-stream --start-maximized --start-fullscreen --disable-desktop-notifications --no-first-run --restore-last-session --disable-session-crashed-bubble https://play.loopsign.eu/hash/$HASH
+chromium-browser --disable-media-stream --start-maximized --start-fullscreen --disable-desktop-notifications --no-first-run --disable-restore-session-state https://play.loopsign.eu/hash/$HASH
